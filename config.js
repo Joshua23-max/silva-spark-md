8c3a7dd355344991ecb6aadeee63e56ca99d664e
Joshua23-max <joshuashereni17@gmail.com>
2025-04-21 12:28:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Silva~g5h0WApb#iBMN1WLT0cpjmT5IoxhmOU4cBipY8fK3oGDYXjaMWfo",
+SESSION_ID: process.env.SESSION_ID || "Silva~H65nTABC#m4aoZIr2_PI850EkwX4YYt4mw_UJyX1mhpciBshxYy8",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
@@ -33,7 +33,7 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 // make true if you want auto reply on status 
